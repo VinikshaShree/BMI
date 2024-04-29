789952ea4d045fe99a00ccddfcd075ca59546eb9
Viniksha_Shree <139342761+VinikshaShree@users.noreply.github.com>
2024-04-29 20:27:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import BmiCalculator from './src';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <BmiCalculator/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
